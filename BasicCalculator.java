845541f11a7ce7a3cc1b8832c4dd962913e6e7c8
Tanzimul Alam Fahim <43729309+tanzimulalam@users.noreply.github.com>
2023-10-01 05:21:43 -0400

    Add files via upload

---
+++ BasicCalculator.java
@@ -0,0 +1,86 @@
+package com.project1;
+import java.util.Scanner;
+
+public class BasicCalculator {
+	public static void main (String args[]) {
+//		TunaMultiClass tunaObject = new TunaMultiClass();
+//		tunaObject.simpleMessage();
+//		int counter = 0;
+//				while (counter  < 11) {
+//					System.out.println(counter);
+//					counter++;
+//		int age;
+//		age = 2;
+//		
+//		switch (age){
+//		case 1: 
+//			System.out.println("You can crowl");
+//			break;
+//		case 2:
+//			System.out.println("You can talk");
+//			break;
+//		case 3:
+//			System.out.println("You can get in trouble");
+//			break;
+//		default:
+//			System.out.println("IDK");
+//			break;
+//			}
+//		int counter;
+//		counter = 0;
+//		while (counter < 10) {
+//			System.out.println(counter);
+//			counter++;
+//			int age;
+//			age = 60;
+//		if (age < 50) {
+//			System.out.println("You are young!");
+//		} else {
+//			System.out.println("You are old!");
+//			if (age > 75) {
+//				System.out.println("You are Really Old!!!!!!");
+//				
+//			} else {
+//				System.out.println("DONT WORRY YOU ARENT REALLY THAT OLD");
+//			}
+//		}
+//		int age;
+//		age = 45;
+//		if (age > 60) {
+//			System.out.println("You are a seniuor citizen!");
+//		} else if (age >=50) {
+//			System.out.println("You are in your 50s");
+//		} else if (age >=40) {
+//			System.out.println("You are in your 40s");
+//		}else {
+//				System.out.println("You are a young buck!");
+//			}
+//		int age;
+//		age = 21;
+//		System.out.println(age > 50 ? "You are old" : "You are young!");
+//		Scanner myScanner = new Scanner(System.in);
+//		int total;
+//		total = 0;
+//		int grade;
+//		int avarage;
+//		int counter;
+//		counter = 0;
+//		
+//		while (counter < 10) {
+//			grade = myScanner.nextInt();
+//			total = total + grade;
+//			counter++;
+//		}
+//		avarage = total / 10;
+//		System.out.println("Your avarage is: " + avarage);
+//		for (int counter = 3; counter <= 21; counter+=2) {
+//			System.out.println(counter);
+//		}
+		int counter;
+		counter = 11;
+		do {
+			System.out.println(counter);
+			counter++;
+		} while (counter <= 10);
+		}
+}
